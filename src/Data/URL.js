9f941185339cd9122b45896d8dfa6dcbe7046914
yogeshlongwani07 <yogeshlongwani07@gmail.com>
2026-02-20 23:42:47 +0530

    fix api url

--- src/Data/URL.js
+++ src/Data/URL.js
@@ -1,5 +1,5 @@
 export const RestroDataAPIUrl =
-  "https://backend-restro-data.onrender.com/api/restro?lat="; //"https://www.swiggy.com/dapi/restaurants/list/v5?lat=";
+  "https://beginners-boutique-accuracy-developers.trycloudflare.com/api/restro?lat="; //"https://www.swiggy.com/dapi/restaurants/list/v5?lat=";
 
 export const GetCityName =
   "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=";
@@ -7,4 +7,5 @@ export const GetCityName =
 export const geocodingUrl =
   "https://geocoding-api.open-meteo.com/v1/search?name=";
 
-export const backendUrl = "https://backend-restro-data.onrender.com";
+export const backendUrl =
+  "https://beginners-boutique-accuracy-developers.trycloudflare.com";
