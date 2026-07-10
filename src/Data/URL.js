e58bf07157f263cff798db03b1046fbeac90437a
Yogesh Longwani <yogeshlongwani07@gmail.com>
2026-07-11 00:13:06 +0530

    Update URL.js

--- src/Data/URL.js
+++ src/Data/URL.js
@@ -1,5 +1,5 @@
 export const RestroDataAPIUrl =
-  "https://beginners-boutique-accuracy-developers.trycloudflare.com/api/restro?lat="; //"https://www.swiggy.com/dapi/restaurants/list/v5?lat=";
+  "https://backend-restro-data.vercel.app/api/restro?lat="; //"https://www.swiggy.com/dapi/restaurants/list/v5?lat=";
 
 export const GetCityName =
   "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=";
@@ -7,5 +7,5 @@ export const GetCityName =
 export const geocodingUrl =
   "https://geocoding-api.open-meteo.com/v1/search?name=";
 
-export const backendUrl =
-  "https://beginners-boutique-accuracy-developers.trycloudflare.com"; //"https://backend-restro-data.onrender.com"; //"http://localhost:5000"; //;
+export const backendUrl = "https://backend-restro-data.vercel.app/";
+  //"https://beginners-boutique-accuracy-developers.trycloudflare.com"; //"https://backend-restro-data.onrender.com"; //"http://localhost:5000"; //;
